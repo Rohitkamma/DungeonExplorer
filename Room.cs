7f040d9924aaf057e2433a446e3259f84a1e7771
rohit <29593068@students.lincoln.ac.uk>
2025-03-13 12:55:24 +0000

    Update Room.cs
    i have reflected on my reviews that i have add more comments

--- Room.cs
+++ Room.cs
@@ -2,15 +2,18 @@
 
 namespace DungeonExplorer
 {
+    // Represents a room in the dungeon with a description
     internal class Room
     {
+        //private field to store the rooms description
         private string description;
-
+        
+        //constructor to itialize the room with a description
         public Room(string description)
         {
             this.description = description;
         }
-
+        //method to retrieve the rooms descripiton
         public string GetDescription()
         {
             return description;
